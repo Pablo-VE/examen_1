d86d3375008827ddbf3ebb7ab64db1e821041028
Pablo-VE <53917989+Pablo-VE@users.noreply.github.com>
2020-10-26 19:57:16 -0600

    Creacion del proyecto

---
+++ proyectos/src/test/java/org/una/examen/proyectos/ProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.una.examen.proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
